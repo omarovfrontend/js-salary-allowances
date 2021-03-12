097d8a6cddf9719983053dc4cbaa6ed719a8331a
Shamil Omarov <s_omarov05@mail.ru>
2021-03-13 02:59:12 +0300

    Поправил ревью

--- js/main.js
+++ js/main.js
@@ -3,17 +3,18 @@ const buttonCalc = document.querySelector('#button');
 
 buttonCalc.addEventListener('click', function() {
     const inputValue = Number(input.value);
-	
-    if(inputValue >= 0 && inputValue <= 3) {
+
+	if(!inputValue) {
+        alert('ERROR!');
+    } else if(inputValue < 3) {
         alert('0%');
-    } else if(inputValue >= 3 && inputValue < 10) {
-      alert('10%');
-    } else if(inputValue >= 10 && inputValue < 20) {
-      alert('20%');
-    } else if(inputValue > 20) {
-      alert('25%');
+    } else if(inputValue < 10) {
+        alert('10%');
+    } else if(inputValue < 20) {
+        alert('20%');
     } else {
-      alert('ERROR!');
+        alert('25%');
     }
+    
     input.value = '';
 });
